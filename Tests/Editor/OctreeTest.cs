450f34c921c64a62faf328ef463f9426fd91c331
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2022-08-03 13:21:05 -0300

    Avanzando las pruebas

--- Tests/Editor/OctreeTest.cs
+++ Tests/Editor/OctreeTest.cs
@@ -5,12 +5,47 @@ using UnityEngine;
 using UnityEngine.TestTools;
 using ItIsNotOnlyMe.SparseOctree;
 
+public class VisitantePrueba : IVisitor<PuntoPrueba>
+{
+    public struct DatosPrueba
+    {
+        public Bounds Limites;
+        public int Profundidad;
+        public int Valor;
+    }
+
+    public List<DatosPrueba> _datos;
+
+    public VisitantePrueba()
+    {
+        _datos = new List<DatosPrueba>();
+    }
+
+    public void Visitar(Nodo<PuntoPrueba> nodo)
+    {
+        DatosPrueba datosPrueba = new DatosPrueba();
+
+        datosPrueba.Limites = nodo.Region;
+        datosPrueba.Profundidad = nodo.Profundidad;
+        datosPrueba.Valor = nodo.Valor.ValorActual;
+
+        _datos.Add(datosPrueba);
+    }
+}
+
 public class OctreeTest
 {
+    private Vector3 _posicion = Vector3.zero;
+    private Vector3 _dimensiones = Vector3.one * 1024;
+
     [Test]
-    public void Test01()
+    public void Test01NoSeInsertaYTieneQueTenerSoloUnaHoja()
     {
-        
+        int profundidad = 3;
+
+        Octree<PuntoPrueba> octree = new Octree<PuntoPrueba>(_posicion, _dimensiones, profundidad);
+
+
     }
 
     /*
---
+++ Tests/Editor/PuntoPrueba.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class PuntoPrueba : IComparable
+{
+    public int ValorActual => _valor;
+
+    private int _valor = 0;
+
+    public int CompareTo(object obj)
+    {
+        PuntoPrueba punto = obj as PuntoPrueba;
+        if (punto._valor == _valor)
+            return 0;
+        return _valor > punto._valor ? 1 : -1;
+    }
+}
