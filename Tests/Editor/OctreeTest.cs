eb5c68488b9f419f6535161f256eff1676fcdb42
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2022-08-03 02:07:34 -0300

    Empezando las pruebas

---
+++ Tests/Editor/OctreeTest.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using ItIsNotOnlyMe.SparseOctree;
+
+public class OctreeTest
+{
+    [Test]
+    public void OctreeTestSimplePasses()
+    {
+        
+    }
+}
