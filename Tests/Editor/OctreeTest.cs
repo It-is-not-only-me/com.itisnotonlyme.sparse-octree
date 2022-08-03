e4eb98cd5a969221278473739fb1d12962253f92
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2022-08-03 02:27:48 -0300

    Dando una idea de las pruebas

--- Tests/Editor/OctreeTest.cs
+++ Tests/Editor/OctreeTest.cs
@@ -8,8 +8,14 @@ using ItIsNotOnlyMe.SparseOctree;
 public class OctreeTest
 {
     [Test]
-    public void OctreeTestSimplePasses()
+    public void Test01()
     {
         
     }
+
+    /*
+     * Hacer una prueba donde se inserta, y por lo tanto deberia solo tener valor la hoja, y nada mas
+     * Hacer una prueba donde se inserta y se elimina, entonces solo esta la raiz sin valor
+     * Hacer una prueba donde se inserta 8 elemento que deberia ir en las 8 hojas, y por lo tanto solo esta la raiz con ese valor 
+     */
 }
